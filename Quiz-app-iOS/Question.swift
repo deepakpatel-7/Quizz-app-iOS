87e84bb219c9b3ebdc59a4a53e19874bd47cf632
deepakpatel-7 <deepakpatel6957@gmail.com>
2023-10-24 15:48:41 +0530

    added functionalities

---
+++ Quiz-app-iOS/Question.swift
@@ -0,0 +1,21 @@
+//
+//  Question.swift
+//  Quiz-app-iOS
+//
+//  Created by Deepak Patel on 24/10/23.
+//
+
+import Foundation
+
+struct Question{
+    let text : String
+    let answer : String
+    
+    init(q:String , a:String){
+        text = q
+        answer = a
+        
+    }
+}
+
+
--- Quiz-app-iOS/ViewController.swift
+++ Quiz-app-iOS/ViewController.swift
@@ -1,20 +1,64 @@
-//
-//  ViewController.swift
-//  Quizzler-iOS13
-//
-//  Created by Angela Yu on 12/07/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
-//
+
 
 import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    
+    let quiz = [
+        Question(q: "JavaScript is a statically-typed language", a: "False"),
+        Question(q: "JavaScript is commonly used for both front-end and back-end web development", a: "True"),
+        Question(q: "JavaScript is a compiled language", a: "False"),
+        Question(q: "The 'let' keyword is used to declare variables with block scope in JavaScript", a:"True"),
+        Question(q: "JavaScript is an object-oriented programming language", a: "True"),
+        Question(q: "The '=== ' operator in JavaScript checks for both value and type equality", a: "True"),
+        Question(q: "In JavaScript, 'null' is the same as 'undefined'", a: "False"),
+        Question(q: "You can use 'break' and 'continue' statements in JavaScript loops", a: "True"),
+        Question(q: "JavaScript allows for multiple inheritance", a: "False"),
+        Question(q: "The 'console.log()' function is used for user input in JavaScript", a: "False"),
+    ];
+
+    
+    var questionNumber  = 0;
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateUI()
     }
-
-
+    
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        let userAnswer = sender.currentTitle;
+        let actualAnswer = quiz[questionNumber].answer
+        
+        if(userAnswer == actualAnswer){
+            sender.backgroundColor = UIColor.green
+        }
+        
+        else{
+            sender.backgroundColor = UIColor.red
+        }
+        
+        if questionNumber+1 < quiz.count{
+            questionNumber+=1
+        }
+        else{
+            questionNumber = 0
+        }
+        
+        Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(updateUI), userInfo:nil, repeats: false)
+    }
+    
+    @objc func updateUI()
+    {
+        questionLabel.text = quiz[questionNumber].text
+        trueButton.backgroundColor = UIColor.clear
+        falseButton.backgroundColor = UIColor.clear
+        progressBar.progress = Float(questionNumber + 1) / Float(quiz.count)
+        
+    }
 }
 
